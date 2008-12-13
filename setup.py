bca26d9fd2d530623946164c77d0fe5bbfdde198
Dan Korostelev <nadako@gmail.com>
2008-12-13 13:46:17 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.breadcrumb',
-    version='1.0.3dev',
+    version='1.0.4dev',
     author = "Roger Ineichen,Stephan Richter and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A pluggable breadcrumbs implementation based on adapters.",
