f9b0a6fe96eaf3a4c543f724fb58efa5cdad9d12
Michael Howitz <mh@gocept.com>
2010-12-12 16:09:19 +0000

    Preparing release 1.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.breadcrumb',
-    version='1.1.1dev',
+    version='1.1.1',
     author = "Roger Ineichen,Stephan Richter and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A pluggable breadcrumbs implementation based on adapters.",
